1629514459569123abd81bc0d95275d607401915
reethika000 <2023ugec073@nitjsr.ac.in>
2026-07-12 06:20:18 +0530

    Update conv.v

--- rtl/conv.v
+++ rtl/conv.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/31/2020 10:09:05 PM
-// Design Name: 
-// Module Name: conv
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module conv(
 input        i_clk,
 input [71:0] i_pixel_data,
