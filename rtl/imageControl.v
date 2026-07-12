cb04663d6b2d74ce3724683186384b4f700ef35b
reethika000 <2023ugec073@nitjsr.ac.in>
2026-07-12 06:20:58 +0530

    Update imageControl.v

--- rtl/imageControl.v
+++ rtl/imageControl.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/01/2020 10:53:27 AM
-// Design Name: 
-// Module Name: imageControl
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 module imageControl(
 input                    i_clk,
 input                    i_rst,
